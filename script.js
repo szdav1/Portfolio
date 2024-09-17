43720bf8ce33aef5423cdf4cbbc736b7b50b8c08
David Szanto <szantodavid.mail@gmail.com>
2024-09-17 09:06:06 +0200

    Make the brief introduction panel.

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 const $ = (id) => {
-    return document.getElementById(id);
+    return document.querySelector(id);
 }
 
-const szurkediv = $("szurke");
-const szabodiv = $("szabo");
-const szantodiv = $("szanto");
+const $s = (id) => {
+    return document.querySelectorAll(id);
+}
+
+const szurkediv = $("#szurke");
+const szabodiv = $("#szabo");
+const szantodiv = $("#szanto");
+const briefIntro = $s(".brief-intro");
+const briefIntroBtn = $s(".brief-intro-btn");
 
 szabodiv.style = "display: none;";
 szantodiv.style = "display: none;";
+
+const getIndexOf = (arr, item) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == item)
+            return i;
+    }
+
+    return -1;
+}
+
+const toggleBriefIntro = (item) => {
+    const index = getIndexOf(briefIntroBtn, item);
+
+    for (let i = 0; i < briefIntroBtn.length; i++) {
+        if (i == index)
+            briefIntro[i].style = "display: block;";
+        else
+            briefIntro[i].style = "display: none;";
+    }
+}
+
+briefIntroBtn.forEach(bib => {
+    bib.addEventListener("click", (e) => {
+        const t = e.target;
+        toggleBriefIntro(t);
+    });
+});
